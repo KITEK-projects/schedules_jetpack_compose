e14cec12aa7abb058c11e9d93787c90743a2376f
XaoriGit <XaoriNIt@gmail.com>
2024-10-28 21:06:38 +0600

    first commit

---
+++ app/src/main/java/com/example/kitekapp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.kitekapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val bg = Color(0xFF272727)
+val bg_item = Color(0xFF343434)
+val gray = Color(0xFF818181)
+val blue = Color(0xFF028BD3)
+val red = Color(0xFFC01F1F)
---
+++ app/src/main/java/com/example/kitekapp/ui/theme/Type.kt
@@ -0,0 +1,26 @@
+package com.example.kitekapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.kitekapp.R
+
+val inter = FontFamily(Font(R.font.inter, FontWeight.Normal))
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = inter,
+        fontSize = 20.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = inter,
+        fontSize = 16.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = inter,
+        fontSize = 14.sp,
+    ),
+)
